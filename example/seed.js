e40c2ca32b2a0b552d10361577a75be3914f01b4
yetist <yetist@cc9c8f76-6a3f-0410-a6b3-db9e14ee52d8>
2011-05-07 13:51:54 +0000

    make release lunar-calendar-2.4.1

--- example/seed.js
+++ example/seed.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env gjs-console
+#!/usr/bin/env seed
 const Gettext = imports.gettext;
 const Gtk = imports.gi.Gtk;
 const LunarCalendar = imports.gi.LunarCalendar;
@@ -10,15 +10,16 @@ function TestCalendar() {
 TestCalendar.prototype = {
   _init: function () {
     this.window = new Gtk.Window ({title: "Lunar Calendar"});
-    this.window.connect("hide", Gtk.main_quit);
+    //this.window.connect("delete-event", Gtk.main_quit);
+    this.window.signal.hide.connect(function () { Gtk.main_quit(); });
     this.cal = new LunarCalendar.Calendar();
     this.window.add(this.cal);
     this.window.show_all ();
   }
 }
 
-Gettext.bindtextdomain("lunar-date", "/usr/share/locale");
-Gettext.textdomain("lunar-date");
+//Gettext.bindtextdomain("lunar-date", "/usr/share/locale");
+//Gettext.textdomain("lunar-date");
 
 Gtk.init (0, null);
 var tc = new TestCalendar();
